0f7ea06c85b6c99fb224124a588670dab4dc0a7d
Michael Shimeles <michaelwasihun96@gmail.com>
2024-08-07 16:54:30 -0400

    Initial

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -3,8 +3,8 @@
 import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { Check, ChevronRight, Circle } from "lucide-react"
+import { cn } from "@/utils/cn"
 
-import { cn } from "@/lib/utils"
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@/utils/cn"
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
