4c97afafb04c6348786145c94198aa7c88e76a86
Michael Shimeles <michaelwasihun96@gmail.com>
2024-08-07 17:37:05 -0400

    Updated blog image

--- content/article.ts
+++ content/article.ts
@@ -2,18 +2,6 @@ import { data_fetching_article } from "./articles/data-fetching";
 import { html_article_rendering } from "./articles/html-redering";
 export const blogs = [
   {
-      id: 0,
-      name: "data fetching in nextjs 14",
-      description: "learn how to be fetch data in nextjs 14",
-      title: "Data Fetching In Nextjs 14",
-      slug: "nextjs-data-fetch",
-      created_at: new Date("8/1/2024").toLocaleString(),
-      link: "/blog/nextjs-data-fetch",
-      article: data_fetching_article,
-      image: "https://utfs.io/f/db400086-1ef0-4a18-9941-6b398789d65d-u8tcx6.png",
-      keywords: []
-  },
-  {
     id: 0,
     name: "rendering articles using html",
     description: "learn how to render a blog using html",
@@ -22,7 +10,19 @@ export const blogs = [
     created_at: new Date("8/7/2024").toLocaleString(),
     link: "/blog/rendering-blog-html",
     article: html_article_rendering,
+    image: "https://utfs.io/f/6acb0fec-d6af-422b-8760-25f1f322368b-x31bch.png",
+    keywords: [],
+  },
+  {
+    id: 1,
+    name: "data fetching in nextjs 14",
+    description: "learn how to be fetch data in nextjs 14",
+    title: "Data Fetching In Nextjs 14",
+    slug: "nextjs-data-fetch",
+    created_at: new Date("8/1/2024").toLocaleString(),
+    link: "/blog/nextjs-data-fetch",
+    article: data_fetching_article,
     image: "https://utfs.io/f/db400086-1ef0-4a18-9941-6b398789d65d-u8tcx6.png",
-    keywords: []
-},
-]
+    keywords: [],
+  },
+];
