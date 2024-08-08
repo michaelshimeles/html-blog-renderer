d49c2a660b641165d04206e80336954d63c7beb6
Michael Shimeles <michaelwasihun96@gmail.com>
2024-08-08 00:11:17 -0400

    Updated card component

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -1,21 +1,44 @@
 "use client"
 import { motion } from "framer-motion"
 import Image from "next/image"
+import { useRef, useState } from "react"
 
-
-export default function BlogCard({ title, description, image }: {
+export default function ProjectCard({ title, description, image }: {
   title: string,
   description: string,
   image: string
 }) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  const [isHovered, setIsHovered] = useState(false)
+
+  function handleMouseMove(event: React.MouseEvent<HTMLDivElement>) {
+    const rect = ref.current?.getBoundingClientRect()
+    if (rect) {
+      const x = event.clientX - rect.left
+      const y = event.clientY - rect.top
+      setMousePosition({ x, y })
+    }
+  }
+
   return (
     <motion.div
-      whileHover={{
-        scale: 1.03,
-        transition: { duration: 0.5 },
-      }}
-      className="w-full rounded-2xl border border-zinc-900 hover:cursor-pointer">
-      <div className="p-3">
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      whileHover={{ scale: 1.03 }}
+      className="relative w-full rounded-2xl border border-zinc-900 hover:cursor-pointer overflow-hidden"
+    >
+      <div
+        className="absolute inset-0 z-0 transition-opacity duration-300 ease-in-out"
+        style={{
+          background: `radial-gradient(circle 150px at ${mousePosition.x}px ${mousePosition.y}px, rgba(255,255,255,0.1), transparent 80%)`,
+          opacity: isHovered ? 1 : 0,
+          pointerEvents: 'none',
+        }}
+      />
+      <div className="relative z-10 p-3">
         <div className="relative w-full aspect-[16/9] rounded overflow-hidden">
           <Image
             src={image}
